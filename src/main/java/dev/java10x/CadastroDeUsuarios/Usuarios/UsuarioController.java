2c5aa770d2dbd93bb2f2302301cdf1d8a71741a2
magnoDuraes <magnodbf962@gmail.com>
2025-08-17 21:19:59 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeUsuarios.Usuarios;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,4 +11,34 @@ public class UsuarioController {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
+    // Adicionar Usuário (CREATE)
+    @PostMapping("/criar")
+    public String criarUsuario(){
+        return "Usuário criado";
+    }
+
+    // Procurar Usuário por ID (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosUsuarios(){
+        return "Mostar Usuário";
+    }
+
+    // Mostrar todos os usuários (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosUsuariosPorId(){
+        return "Mostar Usuário por ID";
+    }
+
+    // Alterar dados dos usuários (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarUsuarioPorId(){
+        return "Mostar Usuário por ID";
+    }
+
+
+    // Deletar Usuário (DELETE)
+    @DeleteMapping("/alterarID")
+    public String deletarUsuarioPorId(){
+        return "Deletar Usuário por ID";
+    }
 }
