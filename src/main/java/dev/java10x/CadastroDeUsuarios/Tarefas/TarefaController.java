8208067b5e3f9b7ab1e8cd4f77d961b744ad013f
magnoDuraes <magnodbf962@gmail.com>
2025-08-17 21:33:10 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/dev/java10x/CadastroDeUsuarios/Tarefas/TarefaController.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Tarefas/TarefaController.java
@@ -1,4 +1,28 @@
 package dev.java10x.CadastroDeUsuarios.Tarefas;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class TarefaController {
+
+    @GetMapping("/")
+    public String listarTarefa(){
+        return "Tarefa listadas com sucesso!";
+    }
+
+    @PostMapping("/criar")
+    public String criarTarefa(){
+        return "Tarefa criada com sucesso!";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarTarefa(){
+        return "Tarefa alterada com sucesso!";
+    }
+
+    @DeleteMapping("/criar")
+    public String deletarTarefa(){
+        return "Tarefa deletada com sucesso!";
+    }
 }
